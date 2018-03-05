89fd3f73f304d9b3e9dd1447a87d7a125f259fdf
Timmy <luili16@126.com>
2018-03-05 17:34:37 +0800

    add

--- exeventbus/src/androidTest/java/com/llx278/exeventbus/ExEventBusTest.java
+++ exeventbus/src/androidTest/java/com/llx278/exeventbus/ExEventBusTest.java
@@ -1,9 +1,18 @@
 package com.llx278.exeventbus;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ServiceTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.runner.intent.IntentStubberRegistry;
+
+import com.llx278.exeventbus.remote.TestService5;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -14,8 +23,26 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class ExEventBusTest {
 
+    private IRouterInteractInterface mTest10;
+    private IRouterInteractInterface mTest11;
+    private IRouterInteractInterface mTest12;
+    private IRouterInteractInterface mTest13;
+
+
+
+    @Rule
+    public final ServiceTestRule mServiceRule = new ServiceTestRule();
+
+
     @Before
-    public void before() {
+    public void before() throws Exception {
+        Context context = InstrumentationRegistry.getTargetContext();
+
+        Intent intent10 = new Intent(context, TestService10.class);
+        IBinder binder10 = mServiceRule.bindService(intent10);
+        mTest10 = IRouterInteractInterface.Stub.asInterface(binder10);
+
+
     }
 
     @After
@@ -27,6 +54,7 @@ public class ExEventBusTest {
      */
     @Test
     public void publishToOtherProcess() {
+
     }
 
     /**
